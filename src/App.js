2e69d089c3b56a866cffb87a3b4f47efbd1c4229
Waylon Dixon <wayjay4@yahoo.com>
2024-04-13 00:20:26 -0700

    implemented both apis for income and balance sheet data retreival and displayed values in list format

--- src/App.js
+++ src/App.js
@@ -4,36 +4,55 @@ import axios from "axios";
 
 function App() {
 
-  const [data, setData] = useState([]);
+    const [incomes, setIncomes] = useState([]);
+    const [balances, setBalances] = useState([]);
 
-  const getFinancialData = () => {
-    const uri = 'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=IBM&apikey=demo';
-    const result =  axios.get(uri)
-        .then((response)=>{
-          console.log('response: ', response.data);
-          setData(response.data);
-          // console.log('data raw: ', response.data.annualReports);
-          // console.log('data: ', data);
-        })
-        .catch((error)=>{
-          console.log('error: ', error);
-        });
-  }
+    const getIncomeData = () => {
+        const uri = 'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=IBM&apikey=demo';
+        const result =  axios.get(uri)
+            .then((response)=>{
+                setIncomes(response.data);
+            })
+            .catch((error)=>{
+                console.log('error: ', error);
+            });
+    }
+
+    const getBalanceData = () => {
+        const uri = 'https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=IBM&apikey=demo';
+        const result =  axios.get(uri)
+            .then((response)=>{
+                setBalances(response.data);
+            })
+            .catch((error)=>{
+                console.log('error: ', error);
+            });
+    }
 
   useEffect(() => {
-    getFinancialData();
+      getIncomeData();
+      getBalanceData();
   }, []);
 
   return (
       <div>
         <h2 className="text-5xl">Hello World</h2>
 
-        <ul>
-          {data.annualReports && data.annualReports.map((report)=> {
-            console.log('report: ', report);
-            return <li>date: {report['fiscalDateEnding']}</li>
-          })}
-        </ul>
+          <ul>
+              {incomes.quarterlyReports && incomes.quarterlyReports.map((report)=> {
+                  const {netIncome, totalRevenue} = report;
+                  return <li>net income: {netIncome}, total revenue: {totalRevenue}</li>
+              })}
+          </ul>
+
+          <hr />
+
+          <ul>
+              {balances.quarterlyReports && balances.quarterlyReports.map((report)=> {
+                  const {totalShareholderEquity} = report;
+                  return <li>share holder equity: {totalShareholderEquity}</li>
+              })}
+          </ul>
       </div>
   );
 }
