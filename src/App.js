7791470d83560580fea1e9860d20f1ec2f9c91b5
Waylon Dixon <wayjay4@yahoo.com>
2024-04-13 00:00:27 -0700

    implemented first api call to get financial report

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  const getFinancialData = () => {
+    const uri = 'https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=IBM&apikey=demo';
+    const result =  axios.get(uri)
+        .then((response)=>{
+          console.log('response: ', response.data);
+          setData(response.data);
+          // console.log('data raw: ', response.data.annualReports);
+          // console.log('data: ', data);
+        })
+        .catch((error)=>{
+          console.log('error: ', error);
+        });
+  }
+
+  useEffect(() => {
+    getFinancialData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h2 className="text-5xl">Hello World</h2>
+
+        <ul>
+          {data.annualReports && data.annualReports.map((report)=> {
+            console.log('report: ', report);
+            return <li>date: {report['fiscalDateEnding']}</li>
+          })}
+        </ul>
+      </div>
   );
 }
 
